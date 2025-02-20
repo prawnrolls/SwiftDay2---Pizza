5b2b0bd6eb999c47f6cad469597248e3503ca98f
Linda Dang <lindadang801@gmail.com>
2025-02-20 12:58:39 +1100

    up to step 7

--- SwiftDay2/ContentView.swift
+++ SwiftDay2/ContentView.swift
@@ -8,17 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var userName:String = ""
+    @State private var selectTomato:Bool = false
+    @State private var selectCheese:Bool = false
+    @State private var selectMushroom:Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("🍕")
+                .font(.system(size: 120))
+            Text("It's pizza time")
+            Form {
+                
+                Section(header: Text("Hello!")) {
+                Section(header: Text("Your name")) {
+                    TextField("Your name", text: $userName, prompt: Text("Your name"))
+                }
+                    
+                    Section(header: Text("Toppings")) {
+                        Toggle("Tomato", isOn: $selectTomato)
+                        Toggle("Cheese", isOn: $selectCheese)
+                        Toggle("Mushroom", isOn: $selectMushroom)}
+                    
+                    //step7
+                    ColorPicker ("the color of your pizza box", selection: .constant(.blue))
+                    
+                }
+            }
+            
         }
         .padding()
     }
+    
 }
-
 #Preview {
     ContentView()
 }
