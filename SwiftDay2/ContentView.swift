68ab8b1180f7bb01789e57524932e41f80ab26f5
Linda Dang <lindadang801@gmail.com>
2025-02-20 11:47:08 +1100

    Initial Commit

---
+++ SwiftDay2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDay2
+//
+//  Created by Linda Dang on 20/2/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftDay2/SwiftDay2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDay2App.swift
+//  SwiftDay2
+//
+//  Created by Linda Dang on 20/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDay2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
