27911bb7b56d2b940f01e8140e2c1b24fbc1417e
Linda Dang <lindadang801@gmail.com>
2025-02-24 13:31:31 +1100

    completed pizza picker

--- SwiftDay2/ContentView.swift
+++ SwiftDay2/ContentView.swift
@@ -14,36 +14,52 @@ struct ContentView: View {
     @State private var selectCheese:Bool = false
     @State private var selectMushroom:Bool = false
     @State private var selectedColor:Color = .blue
+    @State private var orderDate = Date()
+    @State private var pizzaSize:String = "Medium"
     
     var body: some View {
         VStack {
             Text("🍕")
                 .font(.system(size: 120))
-            Text("It's pizza time")
+                .padding(.bottom, 20)
+            Text("It's Pizza time")
+                .fontWeight(.heavy)
+                .font(.system(size: 32))
+            
             Form {
                 
-                //Section(header: Text("Your name for the order")){
-                    
-                    Section(header: Text("Your name")) {
-                        TextField("Your name", text: $userName )
-                    }
-                    
+                Section(header: Text("Your name")) {
+                    TextField("Your name", text: $userName )
+                }
+                
                 Section(header: Text("Toppings")) {
                     Toggle("Tomato", isOn: $selectTomato)
                     Toggle("Cheese", isOn: $selectCheese)
                     Toggle("Mushroom", isOn: $selectMushroom)
                 }
+                
+                Section {
+                    ColorPicker ("The color of your pizza box", selection: $selectedColor)
                     
-                  //  Section (
-                ColorPicker ("the color of your pizza box", selection: $selectedColor)
                 }
                 
+                Section {
+                    Picker("Size" , selection: $pizzaSize) {
+                        ForEach(["Small", "Medium", "Large"], id: \.self) { //create size choices
+                        Text($0)
+                        }
+                    }
+                }
+                
+                Section {
+                    DatePicker("Order date", selection: $orderDate)
+                }
             }
-            
         }
-       // .padding()
     }
-    
+    // .padding()
+}
+
 
 #Preview {
     ContentView()
